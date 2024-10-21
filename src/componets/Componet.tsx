0c7eb3cbfc998d80c2ef414b6116176d98377b3e
MagdalenaMie <madeladesign@gmail.com>
2024-10-21 17:53:49 +0300

    updated types of actions

--- src/componets/Componet.tsx
+++ src/componets/Componet.tsx
@@ -3,16 +3,25 @@ import { useReducer } from "react";
 const Component = () => {
   //   const [count, setCount] = useState<number>(0);
 
-  const ACTIONS ={
-INCREMENT: "increment",
-DECREMENT: "decrement"
-  }
+  const ACTIONS = {
+    INCREMENT: "increment",
+    DECREMENT: "decrement",
+    INCREMENTBYPAYLOAD: "incrementByPayload",
+    MULTIPLYBYPAYLOAD: "multiplyByPayload",
+  } as const;
+
   type State = {
     count: number;
   };
 
 
-  type Action = { type: string } 
+  type Action = { type: typeof ACTIONS.INCREMENT} |
+ { type: typeof ACTIONS.DECREMENT} |
+ { type: typeof ACTIONS.INCREMENTBYPAYLOAD; payload: number } |
+   { type: typeof ACTIONS.MULTIPLYBYPAYLOAD; payload: number } 
+
+
+  
 
   const initialState = { count: 0 };
 
@@ -22,6 +31,10 @@ DECREMENT: "decrement"
         return { count: state.count + 1 };
       case ACTIONS.DECREMENT:
         return { count: state.count - 1 };
+      case ACTIONS.INCREMENTBYPAYLOAD:
+        return { count: state.count + action.payload };
+      case ACTIONS.MULTIPLYBYPAYLOAD:
+        return { count: state.count * action.payload };
       default:
         return state;
     }
@@ -40,6 +53,8 @@ DECREMENT: "decrement"
       <h3>{state.count}</h3>
       <button onClick={() => dispatch({ type: ACTIONS.INCREMENT })}>+</button>
       <button onClick={() => dispatch({ type: ACTIONS.DECREMENT })}>-</button>
+      <button onClick={() => dispatch({ type: ACTIONS.INCREMENTBYPAYLOAD, payload: 7 })}>-7</button>
+      <button onClick={() => dispatch({ type: ACTIONS.MULTIPLYBYPAYLOAD, payload: 5 })}>*5</button>
     </>
   );
 };
